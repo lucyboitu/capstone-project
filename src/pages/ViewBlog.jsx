3b582205b08412586a1e701091c7b7e2bfed5d8e
lucyboitu <tumi565@gmail.com>
2025-05-29 16:55:55 +0200

    Fix: added missing ViewBlog.css

--- src/pages/ViewBlog.jsx
+++ src/pages/ViewBlog.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+// import './ViewBlog.css';
 import './ViewBlog.css';
 
 export default function ViewBlog() {
